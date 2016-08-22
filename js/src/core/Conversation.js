6c03a9089d5d3ff9f9ca1badd1bf9ed3f479f302
igabhishek <igabhishek>
2016-08-22 17:28:36 +0530

    Positioning the exit conversation button

--- js/src/core/Conversation.js
+++ js/src/core/Conversation.js
@@ -30,7 +30,7 @@ BasicGame.Conversation.prototype = {
         this.character1.scale.setTo(-1.5, 1.5);
         this.character2.scale.setTo(1.5, 1.5);
         
-        this.exitButton = this.game.add.button(500, 400, 'playButton', this.switchToGameState, this, 2, 1, 0);
+        this.exitButton = this.game.add.button(Math.floor(this.game.width * 0.8), Math.floor(this.game.height * 0.9), 'playButton', this.switchToGameState, this, 2, 1, 0);
 	},
     
     switchToGameState: function() {
